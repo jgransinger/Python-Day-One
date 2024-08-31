fc40b837a2f74c7e641a20d6a3a9ec2d1ae01728
Joe Gran <132323475+jgransinger@users.noreply.github.com>
2024-08-31 14:54:00 -0500

    Add files via upload

---
+++ task/task.py
@@ -0,0 +1,8 @@
+print("Hello, and welcome to the Band Name Generator App!\n")
+
+city = input("To start, what city are you from?: \n")
+pet = input("What is the name of your pet?: \n")
+
+print("Your band could be named: The " + city + " " + pet + "s")
+print("Or you could use: " + pet + "s" + " of " + city + "!\n")
+
